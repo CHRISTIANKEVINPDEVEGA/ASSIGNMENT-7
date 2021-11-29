58a620ab1effa37d33bee3ab88803ba0a4ab6f19
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2021-11-29 14:01:38 +0800

    -added word count.

--- Program1_Counter.py
+++ Program1_Counter.py
@@ -1,5 +1,66 @@
-def input_name():
-    name=input("Give any senetence or phrase: ")
+Raw_Sentence=input("gve any sentences or phrase: ")
+processed_sentence = Raw_Sentence.lower()
+vowel_count = 0
+consonant_count= 0
 
-input_name()
+for charac in processed_sentence:
+    if charac =='a':
+        vowel_count = vowel_count + 1
+    if charac =='e':
+        vowel_count = vowel_count + 1
+    if charac =='i':
+        vowel_count = vowel_count + 1
+    if charac =='o':
+        vowel_count = vowel_count + 1
+    if charac =='u':
+        vowel_count = vowel_count + 1
     
+
+for charac in processed_sentence:
+    if charac == 'b':
+        consonant_count = consonant_count + 1
+    if charac == 'c':
+        consonant_count = consonant_count + 1        
+    if charac == 'd':
+        consonant_count = consonant_count + 1
+    if charac == 'f':
+        consonant_count = consonant_count + 1
+    if charac == 'g':
+        consonant_count = consonant_count + 1
+    if charac == 'h':
+        consonant_count = consonant_count + 1
+    if charac == 'j':
+        consonant_count = consonant_count + 1
+    if charac == 'k':
+        consonant_count = consonant_count + 1
+    if charac == 'l':
+        consonant_count = consonant_count + 1
+    if charac == 'm':
+        consonant_count = consonant_count + 1
+    if charac == 'n':
+        consonant_count = consonant_count + 1
+    if charac == 'p':
+        consonant_count = consonant_count + 1
+    if charac == 'q':
+        consonant_count = consonant_count + 1
+    if charac == 'r':
+        consonant_count = consonant_count + 1
+    if charac == 's':
+        consonant_count = consonant_count + 1
+    if charac == 't':
+        consonant_count = consonant_count + 1
+    if charac == 'v':
+        consonant_count = consonant_count + 1
+    if charac == 'w':
+        consonant_count = consonant_count + 1
+    if charac == 'x':
+        consonant_count = consonant_count + 1
+    if charac == 'y':
+        consonant_count = consonant_count + 1
+    if charac == 'z':
+        consonant_count = consonant_count + 1
+    
+word_count = vowel_count + consonant_count
+print(f"Word count: {word_count}")
+print(f"Vowel count: {vowel_count}")
+print(f"Consonant count: {consonant_count}")
