7775a685e3b6e273ad946e21ce19320013fb45b5
CHRISTIAN KEVIN P. DE VEGA <49578926+CHRISTIANKEVINPDEVEGA@users.noreply.github.com>
2021-12-03 22:44:07 +0800

    - used def function in the code

--- Program1_Counter.py
+++ Program1_Counter.py
@@ -1,18 +1,27 @@
-Raw_Sentence=input("Input a number of words, sentence, or phrase, and the program will count its elements: ")
-word_count = Raw_Sentence.split()
-processed_sentence = Raw_Sentence.lower()
-vowel_count = 0
-consonant_count= 0
+def Input():
+   Raw_Sentence=input("Input a number of words, sentence, or phrase, and the program will count its elements:\n ")
+   processed_sentence = Raw_Sentence.lower() 
+   return processed_sentence
 
-for charac in processed_sentence:
+def vowel_count(sentence):
+   vowel_count = 0
+   for charac in sentence:
     if charac in 'aeiou':
-        vowel_count = vowel_count + 1   
+        vowel_count = vowel_count + 1
+   print(f"Vowel count: {vowel_count}")
 
-for charac in processed_sentence:
-    if charac in 'bcdfghjklmnpqrstvwxyz':
+def consonant_counter(sentence):
+   consonant_count= 0
+   for charac in sentence:
+     if charac in 'bcdfghjklmnpqrstvwxyz':
         consonant_count = consonant_count + 1
-    
+   print(f"Consonant count: {consonant_count}")
 
-print(f"Word Count:{len(word_count)}")    
-print(f"Vowel count: {vowel_count}")
-print(f"Consonant count: {consonant_count}")
+def word_counter(sentence):
+   word_count = sentence.split()
+   print(f"Word Count:{len(word_count)}")    
+
+sentence = Input()
+word_counter(sentence)
+vowel_count(sentence) 
+consonant_counter(sentence)
