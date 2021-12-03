7f8e6674737e5d6c7a47069eda51d75e569bbddb
CHRISTIAN KEVIN P. DE VEGA <49578926+CHRISTIANKEVINPDEVEGA@users.noreply.github.com>
2021-12-03 23:34:50 +0800

    -minor changes in code again

--- Program1_Counter.py
+++ Program1_Counter.py
@@ -3,25 +3,25 @@ def Input():
    processed_sentence = Raw_Sentence.lower() 
    return processed_sentence
 
-def vowel_counter(sentence):
+def vowel_counter(V_sentence):
    vowel_count = 0
-   for charac in sentence:
+   for charac in V_sentence:
     if charac in 'aeiou':
         vowel_count = vowel_count + 1
    print(f"Vowel count: {vowel_count}")
 
-def consonant_counter(sentence):
+def consonant_counter(C_sentence):
    consonant_count= 0
-   for charac in sentence:
+   for charac in C_sentence:
      if charac in 'bcdfghjklmnpqrstvwxyz':
         consonant_count = consonant_count + 1
    print(f"Consonant count: {consonant_count}")
 
-def word_counter(sentence):
-   word_count = sentence.split()
+def word_counter(W_sentence):
+   word_count = W_sentence.split()
    print(f"Word Count:{len(word_count)}")    
 
-sentence = Input()
-word_counter(sentence)
-vowel_counter(sentence) 
-consonant_counter(sentence)
+SENTENCE = Input()
+word_counter(SENTENCE)
+vowel_counter(SENTENCE) 
+consonant_counter(SENTENCE)
