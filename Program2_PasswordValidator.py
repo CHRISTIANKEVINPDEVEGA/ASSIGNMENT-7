aecb2993158460cb2058f1654d34f6fc451abe22
CHRISTIAN KEVIN P. DE VEGA <49578926+CHRISTIANKEVINPDEVEGA@users.noreply.github.com>
2021-12-03 23:31:37 +0800

    - use def function in the code

--- Program2_PasswordValidator.py
+++ Program2_PasswordValidator.py
@@ -1,16 +1,21 @@
 import re
-while True:
- user_password=input("Please input you new password: ")
- if (re.search(r"[a-z]",user_password)) is None:
-   print("Invalid password, your password should contain atleast 1 alphabetic character in lowercase.")
- elif (re.search(r"[A-Z]",user_password)) is None:
-   print("Invalid password, your password should contain atleast 1 alphabetic character in uppercase.")
- elif (re.search(r"[\d]",user_password)) is None:
-   print("Invalid password, your password should contain atleast 1 number.")
- elif (re.search(r"[!@#$%^&*()_+/-=]",user_password)) is None:
-   print("Invalid password, your password should contain atleast 1 special character.")
- elif (len(user_password) < 16 ):
-   print("Invalid password, your password should be greater than 15 chracters in length.")
- else:
-   print("Valid password, you have successfully created your won password.")
-   break
+
+
+def system():
+   while True:
+    user_password = input("Please input you new password: ")
+    if (re.search(r"[a-z]",user_password)) is None:
+       print("Invalid password, your password should contain atleast 1 alphabetic character in lowercase.")
+    elif (re.search(r"[A-Z]",user_password)) is None:   
+       print("Invalid password, your password should contain atleast 1 alphabetic character in uppercase.")       
+    elif (re.search(r"[\d]",user_password)) is None:
+       print("Invalid password, your password should contain atleast 1 number.")     
+    elif (re.search(r"[!@#$%^&*()_+/-=]",user_password)) is None:
+       print("Invalid password, your password should contain atleast 1 special character.")      
+    elif (len(user_password) < 16 ):      
+       print("Invalid password, your password should be greater than 15 chracters in length.")                                                       
+    else:
+       print("Valid password, you have successfully created your won password.")
+       break
+  
+system()
