8fd7837dc9e78cdc47d297e507d8ee45a7b49da2
CHRISTIAN KEVIN P. DE VEGA <49578926+CHRISTIANKEVINPDEVEGA@users.noreply.github.com>
2021-12-03 21:12:37 +0800

    -fixed an error in the coding, where the program counts the number of letters instead of words, - added some text

--- Program1_Counter.py
+++ Program1_Counter.py
@@ -1,4 +1,5 @@
-Raw_Sentence=input("gve any sentences or phrase: ")
+Raw_Sentence=input("Input a number of words, sentence, or phrase, and the program will count its elements: ")
+word_count = Raw_Sentence.split()
 processed_sentence = Raw_Sentence.lower()
 vowel_count = 0
 consonant_count= 0
@@ -59,7 +60,7 @@ for charac in processed_sentence:
         consonant_count = consonant_count + 1
     elif charac == 'z':
         consonant_count = consonant_count + 1
-    
-print(f"Word count: {len(processed_sentence)}")
+
+print(f"Word Count:{len(word_count)}")    
 print(f"Vowel count: {vowel_count}")
 print(f"Consonant count: {consonant_count}")
