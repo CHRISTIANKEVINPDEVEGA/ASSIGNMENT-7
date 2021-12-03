8a6b6a7622c9333b2ebeba5fadc3a7ad9e642b5b
CHRISTIAN KEVIN P. DE VEGA <49578926+CHRISTIANKEVINPDEVEGA@users.noreply.github.com>
2021-12-03 20:51:59 +0800

    -added more special characters, -changed some text

--- Program2_PasswordValidator.py
+++ Program2_PasswordValidator.py
@@ -7,10 +7,10 @@ while True:
    print("Invalid password, your password should contain atleast 1 alphabetic character in uppercase.")
  elif (re.search(r"[\d]",user_password)) is None:
    print("Invalid password, your password should contain atleast 1 number.")
- elif (re.search(r"[!@#$%^&*()_+]",user_password)) is None:
+ elif (re.search(r"[!@#$%^&*()_+/-=]",user_password)) is None:
    print("Invalid password, your password should contain atleast 1 special character.")
  elif (len(user_password) < 16 ):
    print("Invalid password, your password should be greater than 15 chracters in length.")
  else:
-   print("Valid password, you successfully created your password.")
+   print("Valid password, you have successfully created your won password.")
    break
